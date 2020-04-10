d466daaf25472e19abba6f3bde2357af55d67246
carolhm92 <47649555+carolhm92@users.noreply.github.com>
2020-04-09 21:42:26 -0600

    mas cambios de citas

---
+++ Consola/Consola/Helpers/SessionManage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Consola.Helpers
+{
+    public class SessionManage : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            HttpContext context = HttpContext.Current;
+
+            if (HttpContext.Current.Session["US"] == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary
+                {
+                    {"action", "Login" },
+                    {"controller", "Login"},
+                    {"returnUrl", filterContext.HttpContext.Request.RawUrl }
+                });
+                return;
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+
+    }
+}
---
+++ Consola/Consola/Models/Cita.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Consola.Models
+{
+    public class Cita
+    {
+        public int IdCita { get; set; }
+        public string Asunto { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Inicio { get; set; }
+        public DateTime Fin { get; set; }
+        public string ColorFondo { get; set; }
+        public bool DiaCompleto { get; set; }
+
+    }
+}
